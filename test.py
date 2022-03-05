a9b79f1aefd6ef1eccb51d22bfab574a3df68cde
pwiener <patryk.wiener@siroccomobile.com>
2022-03-05 12:29:54 +0100

    pep8 test

---
+++ test.py
@@ -0,0 +1,115 @@
+"""
+This module contains ...
+"""
+import time
+import compileall
+
+def func():
+    pass
+
+
+MY_SUPER_CONST = (
+    'abcabcbcabcabcbcabcabcbcabcabcbcabcabcbcabcabcbcabcabcbcabcabcbcabcabcbcabcabcbcabcabcbcabcabcbcabcabcbcabcabcbcc'
+    'bdbcdbdcb'
+)
+MY_SUPER_CONST = 'abcabcbcabcabcbcabcabcbcabcabcbcabcabcbcabcabcbcabcabcbcabcabcbcabcabcbcabcabcbcabcabcbcabcabcbcabc' \
+                 'abcbcabcabcbccbdbcdbdcb'
+
+income = (gross_wages +
+          taxable_interest +
+          (dividends - qualified_dividends) -
+          ira_deduction -
+          student_loan_interest)
+
+income = (gross_wages
+          + taxable_interest
+          + (dividends - qualified_dividends)
+          - ira_deduction
+          - student_loan_interest)
+
+
+class Sample1:
+    """
+    This is sample class for ...
+    """
+
+    def method1(self):
+        """
+
+        :return:
+        """
+        pass
+
+    @classmethod
+    def method2(cls):
+        pass
+
+
+class Sample2:
+    pass
+
+
+def func1():
+    pass
+
+
+def func2(sample_param):
+    """
+    This function print Ala kota.
+
+    :param sample_param: string containing...
+    :return: None
+    """
+    print('Ala')
+    print('kota')
+
+
+ARRAY = ['Ala', 'ma', 'kota']
+
+ARRAY = [
+    'Ala',
+    'ma',
+    'kota',
+]
+
+# ham[lower+offset : upper+offset]
+
+x = 1
+y = 2
+long_variablelong_variablelong_variablelong_variablelong_variablelong_variable = 3
+
+initialize(
+    FILES,
+    error=True,
+)
+
+x = x + 1  # Compensate for border
+
+l = 0
+I = 0
+
+O = 0
+
+if True:
+    if True:
+        if True:
+            if True:
+        elif True:
+            pass
+    else:
+        pass
+
+
+def func3():
+    my_dict = {
+        'a': 'kot',
+        'b': 'pies',
+    }
+
+    variable = 'a'
+    if variable == 'a':
+        return 'kot'
+    elif variable == 'b':
+        return 'pies'
+
+    return my_dict[variable]
