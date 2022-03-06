0ca35d722dcedbebe2c81fb104bcf4a91ca3ce9f
pwiener <patryk.wiener@siroccomobile.com>
2022-03-06 13:19:44 +0100

    OCP

--- solid/open_closed_principle.py
+++ solid/open_closed_principle.py
@@ -1,21 +1,50 @@
-class EasterDiscount:
+from abc import abstractmethod, ABC
+
+
+class Discount(ABC):
+    @abstractmethod
+    def get_discount(self):
+        pass
+
+
+class EasterDiscount(Discount):
     _EASTER_DISCOUNT = '30%'
 
-    def get_easter_discount(self):
+    def get_discount(self):
         return f'{self._EASTER_DISCOUNT} on Easter'
 
 
-class BlackFridayDiscount:
+class BlackFridayDiscount(Discount):
     _BLACK_FRIDAY_DISCOUNT = '50%'
 
-    def get_black_friday_discount(self):
+    def get_discount(self):
         return f'{self._BLACK_FRIDAY_DISCOUNT} on Black Friday'
 
 
+class WomenDayDiscount(Discount):
+    _WOMEN_DAY_DISCOUNT = '25%'
+
+    def get_discount(self):
+        return f'{self._WOMEN_DAY_DISCOUNT} on Women Day'
+
+
 class DiscountManager:
+    def process_discount(self, discount: Discount):
+        discount.get_discount()
 
-    def process_easter_discount(self, easter_discount: EasterDiscount):
-        pass
 
-    def process_black_friday_discount(self, black_friday_discount: BlackFridayDiscount):
-        pass
+if __name__ == '__main__':
+    women_day_discount = WomenDayDiscount()
+    black_friday_discount = BlackFridayDiscount()
+
+    discount_manager = DiscountManager()
+    discount_manager.process_discount(discount=women_day_discount)
+    discount_manager.process_discount(discount=black_friday_discount)
+
+
+class Building:
+    pass
+
+
+class Flat:
+    pass
